e0a28ce44d65c8d33875f802c627709dd833b3d8
aditya-renake <aditya.renake@outlook.com>
2026-08-31 23:52:34 +0530

    loading

--- loading.py
+++ loading.py
@@ -1,14 +1,11 @@
 import cv2
 image = cv2.imread("avr.jpeg")
 
-if image is not None:
-    cv2.imshow("Image Showing", image) #open the window
-    cv2.waitKey(0) #wait for a key
-    cv2.destroyAllWindows() #closes all the windows
-    print("Error imagge not found") #close the window
+if image is None:
+    print("Error image not found") 
 
 else:
-    print("Could not load the image")
+    print("Image loaded succesfully ")
 
 
 #image = cv2.imread("imagename.jpg", flag)
