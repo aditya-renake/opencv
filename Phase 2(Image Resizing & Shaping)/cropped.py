c1aeb616d1d45d8406ae06dda602babee18fbc78
aditya-renake <aditya.renake@outlook.com>
2026-09-03 17:08:38 +0530

    cropped

--- Phase 2(Image Resizing & Shaping)/cropped.py
+++ Phase 2(Image Resizing & Shaping)/cropped.py
@@ -1,20 +1,17 @@
 import cv2
-
 image = cv2.imread("Phase 2(Image Resizing & Shaping)/avr.jpeg")
-#(width, height)
 
 if image is None:
     print("Image not found")
-
 else:
     print("Image loaded")
 
-    resized = cv2.resize(image, (300, 300)) #always pass dimesions in tuple
-
+    cropped = image[100:400, 100:400] #cropping the image
+    
     cv2.imshow("Original image", image)
-    cv2.imshow("Resized image", resized)
+    cv2.imshow("Cropped image", cropped)
 
-    cv2.imwrite("resized image.png", resized)
+    cv2.imwrite("cropped image.png", cropped)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
